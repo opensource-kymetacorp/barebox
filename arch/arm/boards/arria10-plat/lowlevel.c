c92f0c7dfa3a358443acb2b20ec1ef099918517b
Kymeta Corp <opensource@kymetacorp.com>
2018-02-15 13:01:24 -0800

    arria10: Add minimal dts for xload build
    Arria 10 xload will run from OCRAM (256K) and eventually program
    the fpga.  Once programmed, access to the memory controller and
    main memory is available.  The the second stage of barebox will
    load the OS, similar to the Cyclone 5 operation with xload.

--- arch/arm/boards/arria10-plat/lowlevel.c
+++ arch/arm/boards/arria10-plat/lowlevel.c
@@ -15,8 +15,29 @@
 #include <mach/generic.h>
 
 extern char __dtb_socfpga_arria10_plat_start[];
+extern char __dtb_socfpga_arria10_plat_xload_start[];
 
 #ifdef CONFIG_ARCH_SOCFPGA_XLOAD
+static noinline void arria10_plat_entry(void)
+{
+	void *fdt;
+
+	arm_early_mmu_cache_invalidate();
+	relocate_to_current_adr();
+	setup_c();
+	arria10_init(&mainpll_cfg, &perpll_cfg, pinmux);
+	puts_ll("lowlevel init done\n");
+
+	fdt = __dtb_socfpga_arria10_plat_xload_start - get_runtime_offset();
+	barebox_arm_entry(0xffe00000, SZ_256K, fdt);
+}
+
+ENTRY_FUNCTION(start_socfpga_arria10_plat_xload, r0, r1, r2)
+{
+	arm_cpu_lowlevel_init();
+	arm_setup_stack(0xffe00000 + SZ_256K - SZ_32K - SZ_4K - 16);
+	arria10_plat_entry();
+}
 
 #else
 
